8b527a92ed37def07ae1f3644fa1f3c795820d86
Sahilll10 <sahilgreat321@gmail.com>
2025-07-06 08:29:14 +0000

    StackHeap

---
+++ devCon/01_basics/StackHeap.js
@@ -0,0 +1,20 @@
+// Stack (Primitive), Heap (Non-Primitive)
+
+let myYoutubeName = "hiteshchoudharydotcom";
+
+let anotherName = myYoutubeName;
+anotherName = "chaiaurcode";
+
+console.log(myYoutube);
+console.log(anothername)
+
+let UserOne={
+   email: "user@gmail.com"
+   upi: "sahil@okaxis"
+   }
+
+let userTwo= UserOne
+userTwo.email = "sahil@google.com"
+
+console.log(UserOne.email)
+console.log(UserTwo.email)
